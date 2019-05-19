2d2fef2adcc8e8747a8b8114fbb9d92a2c3caa4c
Stephan <stephan@Stephans-MacBook-Pro.local>
2019-05-19 22:01:42 +0200

    Comments added

--- modules/fibonacci.py
+++ modules/fibonacci.py
@@ -1,9 +1,20 @@
 class FibonacciNumbers:
+    """Calulates the fibonacci sequence
+    F(n) = F(n-1) + F(n-2) for n>1
+
+    Args:
+        arg n (int) the last possible number in fibonacci series
+    """
 
     def __init__(self, n):
         self.n = n
 
     def calc(self):
+        """Calculates the fibonacci series up to a specific number
+
+        Returns:
+            List of fibonacci numbers
+        """
         result = []
         a, b = 0, 1
         while a < self.n:
